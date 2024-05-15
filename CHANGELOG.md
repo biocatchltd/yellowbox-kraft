3f8a3e5ade3dcb3f3dc3ffd13fff047185caf072
Ben Avrahami <wouldntyouliketoknow@weather.boy>
2024-05-15 17:08:35 +0300

    ci

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 # Yellowbox-Kraft Changelog
+## 0.1.1
+### Added
+* support for python 3.12
+### Removed
+* support for python 3.7
 ## 0.1.0
 * initial
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # run various linters
 set -e
-echo "running black..."
-python -m black .
+echo "formatting..."
+python -m ruff format .
 echo "sorting import with ruff..."
-python -m ruff . --select I,F401 --fix --show-fixes
+python -m ruff check . --select I,F401 --fix --show-fixes
